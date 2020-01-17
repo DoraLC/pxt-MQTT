39a79d542e7cf41ee88230e61338154530e34f8c
moore007 <38647272+moore007@users.noreply.github.com>
2020-01-17 12:03:59 +0800

    Update main.ts

--- main.ts
+++ main.ts
@@ -188,7 +188,7 @@ namespace MQTT {
         let ThingSpeakTopic: string = "channels/" + id + "/publish/" + api;
         let payload: string = "";
         for (let i = 0; i < fields.length; i++) {
-            while(field[i] != null){
+            while(fields[i] != null){
                 payload += "field" + (i + 1) + "=" + fields[i] + "&";
             }
         }
